1090c46267cd67d69a176705a19ef494ef70058d
agustin rosano <arosano@goldmund.com>
2025-11-18 13:12:59 -0300

    primer comit de strapi projecto master

---
+++ config/middlewares.ts
@@ -0,0 +1,21 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+      ],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
