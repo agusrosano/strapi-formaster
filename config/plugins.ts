b26ac1a6d1f6669eea89e057da1b5f863b58a680
agus <arosano@goldmund.com.ar>
2025-11-19 17:12:26 -0300

    Actualizaciones

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config:
+      env('UPLOAD_PROVIDER', 'local') === 'gcs'
+        ? {
+            provider: 'strapi-provider-upload-google-cloud-storage',
+            providerOptions: {
+              bucketName: env('GCS_BUCKET_NAME'),
+              basePath: env('GCS_BASE_PATH', ''),
+              publicFiles: env.bool('GCS_PUBLIC_FILES', true),
+              uniform: env.bool('GCS_UNIFORM', true),
+            },
+          }
+        : {},
+  },
+});
